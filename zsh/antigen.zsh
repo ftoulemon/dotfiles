d5297a7c49e3be1887589ba09a8595e2e6719265
ftoulemon <florentin.toulemon@gmail.com>
2017-01-07 23:34:42 +0100

    Add antigen

---
+++ zsh/antigen.zsh
@@ -0,0 +1,7 @@
+source $HOME/.dotfiles/antigen/antigen.zsh
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+
+antigen apply
