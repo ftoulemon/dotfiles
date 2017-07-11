e963be0a7eba7e74cee2d032cb09f725906f393d
ftoulemon <florentin.toulemon@gmail.com>
2017-07-11 13:08:46 +0200

    Update prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,10 +2,10 @@ ZSH_THEME=""
 autoload -U promptinit
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git cvs svn
-zstyle ':vcs_info:*' formats '[(%s) %b%u%c]'
-zstyle ':vcs_info:*' actionformats '[(%s) %b%u%c]'
-zstyle ':vcs_info:*' unstagedstr ' +'
-zstyle ':vcs_info:*' stagedstr ' o'
+zstyle ':vcs_info:*' formats '[(%s) %b%u%c]'
+zstyle ':vcs_info:*' actionformats '[(%s) %b%u%c]'
+zstyle ':vcs_info:*' unstagedstr '  '
+zstyle ':vcs_info:*' stagedstr '  '
 zstyle ':vcs_info:*' check-for-changes true
 promptinit
 prompt adam2
@@ -18,6 +18,8 @@ precmd() {
 	fi
 }
 
-prompt_line_1a='%B%F{cyan}.%b%F{cyan}-%B%F{black}[%B%F{green}%~%B%F{black}]%b%F{gray}$(virtualenv_prompt_info)%b%F{blue}${vcs_info_msg_0_}%B%F{black}%b%F{cyan}'
+prompt_line_1a='%b%F{cyan}┌─%B%F{black}[%B%F{green}%~%B%F{black}]%b%F{gray}$(virtualenv_prompt_info)%b%F{blue}${vcs_info_msg_0_}%B%F{black}%b%F{cyan}'
 prompt_line_1b='%B%F{black}[%T][%b%F{cyan}%n%B%F{cyan}@%b%F{cyan}%m%B%F{black}]%b%F{cyan}'
+prompt_line_2='%b%F{cyan}└──%B%F{white}'
+prompt_char='%(!.#.)'
 
