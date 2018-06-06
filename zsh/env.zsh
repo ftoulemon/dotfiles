5ea2785ae1a432fc709d5cf00822b9770eb02f21
ftoulemon <florentin.toulemon@gmail.com>
2018-06-06 09:49:17 +0800

    Fix zsh env vars

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,12 @@
-# Customize to your needs...
+# hbox
 export PYTHONPATH=:./src
+# GO path
 export PATH=$PATH:/usr/local/go/bin
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+# Xilinx SDK
+export PATH=/opt/Xilinx/SDK/2016.4/gnu/arm/lin/bin/:$PATH
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+
+# ROS
+source /opt/ros/melodic/setup.zsh
 
