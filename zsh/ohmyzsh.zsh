1a1f4078b304cb4622bc5108eca360c844cbb40f
ftoulemon <florentin.toulemon@gmail.com>
2018-10-29 13:38:20 +0800

    git: add git-extras

--- zsh/ohmyzsh.zsh
+++ zsh/ohmyzsh.zsh
@@ -11,7 +11,7 @@ if [ ! -d $ZSH ]; then
     fi
 fi
 
-plugins=(git colored-man web-search colorize docker cp sudo thefuck tmux screen virtualenv ubuntu python z zsh-navigation-tools)
+plugins=(git git-extras colored-man web-search colorize docker cp sudo thefuck tmux screen virtualenv ubuntu python z zsh-navigation-tools)
 
 source $ZSH/oh-my-zsh.sh
 
